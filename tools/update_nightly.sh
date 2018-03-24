959f31ea1b4bcd310611a2243bbafaa869da24ae
Jakub Fijałkowski <fiolek94@gmail.com>
2018-03-24 14:17:00 +0100

    Fix `update_nightly.sh`

--- tools/update_nightly.sh
+++ tools/update_nightly.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev
+set -e
 if [ "$UPDATE_NIGHTLY" = "YES" ]; then
     SNAPSHOTS="$(curl -s "https://s3.amazonaws.com/haddock.stackage.org/snapshots.json")"
     LATEST_VER="$(echo "$SNAPSHOTS" | grep -oP 'nightly-....-..-..')"
